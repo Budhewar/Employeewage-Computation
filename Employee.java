04c2dd215a3a18fc274bac7ee318f7dddf54c88c
Budhewar <anuradhabudhewar@gmail.com>
2022-12-01 13:46:17 +0530

    Employee is present or absent

--- Employee.java
+++ Employee.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.emp;
 
+import java.util.Random;
+
 public class Employee {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation");
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+		if (attendance == 1) {
+			System.out.println("Employee is present");
+
+		} else {
+			System.out.println("Employee is absent");
+		}
 
 	}
 
